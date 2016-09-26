7a190fd5f6077a13a6284f39bb0b2b7f3d975266
rsimari <rsimari@nd.edu>
2016-09-26 18:57:08 -0400

    started qsort

--- quick.cpp
+++ quick.cpp
@@ -13,12 +13,20 @@ Node *concatenate(Node *left, Node *right);
 // Implementations
 
 void quick_sort(List &l, bool numeric) {
+
 }
 
 Node *qsort(Node *head, CompareFunction compare) {
+	if (head->next == nullptr || head->next->next == nullptr) return;
+	// call partition here
+	Node *pivot = *head;
 }
 
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, CompareFunction compare) {
+	pivot = head;
+	while (left != right) {
+
+	}
 }
 
 Node *concatenate(Node *left, Node *right) {
