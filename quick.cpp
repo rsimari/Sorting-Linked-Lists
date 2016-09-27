8bee2a369145cfb0f980576a8e8265defce07943
rsimari <rsimari@nd.edu>
2016-09-26 20:38:12 -0400

    finished qsort

--- quick.cpp
+++ quick.cpp
@@ -13,25 +13,31 @@ Node *concatenate(Node *left, Node *right);
 // Implementations
 
 void quick_sort(List &l, bool numeric) {
-
+	Node *head;
+	if (numeric) head = qsort(l.head, compare_numeric);
+	else head = qsort(l.head, compare_string);
+	l.head = head;
 }
 
 Node *qsort(Node *head, CompareFunction compare) {
-	if (head->next == nullptr || head->next->next == nullptr) return head;
-	// call partition here
-	Node *pivot = head;
+	if (head->next == nullptr) return head;
+	// partition
+	Node *l, *r;
+	partition(head, head, l, r, compare);
+	l = qsort(l, compare);
+	r = qsort(r, compare);
+	Node *newHead = concatenate(l, r);
 	return head;
 }
 
 void partition(Node *head, Node *pivot, Node *&left, Node *&right, CompareFunction compare) {
 	pivot = head;
-	while (left != right) {
-		return;
-	}
+	
 	return;
 }
 
 Node *concatenate(Node *left, Node *right) {
+
 }
 
 // vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
